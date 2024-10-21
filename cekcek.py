fdc251473b2a5fb21ddc3de21fd800b9e6eb4a21
Pintar Ngoding <88563676+pintarngoding@users.noreply.github.com>
2024-10-21 12:17:10 +0700

    Update cekcek.py

--- cekcek.py
+++ cekcek.py
@@ -1,18 +1,3 @@
-from turtle import *
-import time
-
-bgcolor('black')
-pencolor('#F69441')
-width(23)
-penup()
-goto(160, -100)
-pendown()
-
-left(90)
-for i in range(4):
-    forward(250)
-    circle(34, 90)
-
 penup()
 goto(85, 30)
 pendown()
